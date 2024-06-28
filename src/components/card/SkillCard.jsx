9d9fbd2f938d4b9d88a0034f7bffb2ecf6f29877
Kanwar07 <anmolkanwar07@gmail.com>
2024-06-28 14:49:06 +0530

    font

--- src/components/card/SkillCard.jsx
+++ src/components/card/SkillCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SkillCard({ skill, description, color, textcolor }) {
   return (
     <div className="group">
-      <div className="flex justify-center border border-[2px] border-[#000000] bg-[#ed6955] text-[#ffffff] px-6 py-4 rounded-lg cursor-pointer transition ease-in-out duration-[400ms] hover:scale-105">
+      <div className="flex justify-center border border-[2px] border-[#000000] bg-[#ed6955] text-[#000000] px-6 py-4 rounded-lg cursor-pointer transition ease-in-out duration-[400ms] hover:scale-105">
         {skill}
       </div>
       <div
